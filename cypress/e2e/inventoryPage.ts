45cb68b1fa7bfce3dab715cdd02a63afce1e37e9
tranev95 <trajchepaypal@yahoo.com>
2024-10-10 14:31:43 +0200

    The login flow now works

--- cypress/e2e/inventoryPage.ts
+++ cypress/e2e/inventoryPage.ts
@@ -1,17 +1,9 @@
 import { constants, elements, flows } from '../support'
 
 describe('Login to the page using the standard user, and assert the elements visible on the inventory page', () => {
- /* For some reason the page goes blank if we use Before
   before(() => {
     flows.loginStandardUser()
   })
-*/it('Login as standard user', () => {
-  cy.visit(constants.pageUrl.loginPage)
-	cy.get(elements.loginPageData.usernameField).type('standard_user')
-    cy.get(elements.loginPageData.passwordField).type('secret_sauce')
-    cy.get(elements.loginPageData.loginButton).click()
-    cy.url().should('include', '/inventory')
-})
   it('Locate the Page Logo Title, assert the text', () => {
     cy.get(elements.inventoryPageData.pageLogo).should('be.visible').and('contain.text', 'Swag Labs')
   })
